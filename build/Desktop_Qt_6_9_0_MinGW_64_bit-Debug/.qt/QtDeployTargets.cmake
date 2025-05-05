4c2dc1f049cead477d786cc5429ca94c44beebe6
Jair Saldaña <129416494+jairsl2206@users.noreply.github.com>
2025-05-05 11:00:54 -0600

    Delete build directory

--- build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Charifas_FILE C:/Users/Jair S/Documents/Charifas/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/Charifas.exe)
-set(__QT_DEPLOY_TARGET_Charifas_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Charifas_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6SerialPort.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Charts.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6OpenGLWidgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6OpenGL.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
